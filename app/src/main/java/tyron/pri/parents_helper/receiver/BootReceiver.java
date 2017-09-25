f1bf2a5382669a8eb0f7aed45c3db80c55df3434
tyronfong <tyronfong@outlook.com>
2017-09-26 01:29:34 +0800

    add boot receiver

---
+++ app/src/main/java/tyron/pri/parents_helper/receiver/BootReceiver.java
@@ -0,0 +1,17 @@
+package tyron.pri.parents_helper.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by tyron on 9/26/2017.
+ */
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
